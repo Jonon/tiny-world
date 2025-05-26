aaa20c2c5277fba3c0a67fd551a0fdd7c4f46e0e
Jonathan <jonathancjonsson@gmail.com>
2025-05-26 20:06:05 +0200

    Add player animation on key push
    - Flip sprite on left and right arrow press
    - Play animation on jump
    - Play walk animation on walking left and right

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
-
 const SPEED = 110.0
 const JUMP_VELOCITY = -250.0
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -15,8 +15,25 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("walk_left", "walk_right")
+
+	# flip the sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif  direction < 0:
+		animated_sprite.flip_h = true
+
+	if is_on_floor():
+	# Play animation
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("walk")
+	else:
+		animated_sprite.play("jump")
+
+
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
